77440d2c8b93207b18050790e3055d5d6001d5b5
Vinicius Brito <viniciusbrito0511@gmail.com>
2025-02-20 20:24:24 -0300

    Add files via upload

---
+++ banco de dados.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE bd_escola;
+
+use bd_escola;
+
+CREATE TABLE tb_aluno (
+ cd_aluno INT AUTO_INCREMENT PRIMARY KEY,
+ nm_aluno VARCHAR(45),
+ cpf VARCHAR(11)
+);
+
+CREATE TABLE tb_aluno (
+ cd_aluno INT AUTO_INCREMENT PRIMARY KEY,
+ nm_aluno VARCHAR(45),
+ cpf VARCHAR(11)
+
+create user 'Danielle'@'localhost' identified by '1234';
+grant all privileges on *.* to 'Danielle'@'localhost';
+
+/*Usuario com permissão apenas na database bd_escola/
+create user 'Graciete'@'localhost' identified by '1234';
+grant all privileges on tb_aluno.* to 'Graciete'@'localhost';
+
+/*Usuario com permisssão apenas para executar o comando select database bd_escola*/
+create user 'JoseCarlos'@'localhost' identified by '1234';
+grant select on tb_aluno.* to 'JoseCarlos'@'localhost';
